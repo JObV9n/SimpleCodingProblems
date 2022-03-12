ce9cc74a9775c904f17f9090162819f9ae3b0f29
JObV9n <57562946+JObV9n@users.noreply.github.com>
2022-03-12 19:16:29 +0545

    Update basicJavainput.java

--- basicJavainput.java
+++ basicJavainput.java
@@ -1,6 +1,7 @@
 //In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below
 
 //Input Format
+//1 2 3
   //There are  lines of input, and each line contains a single integer.
 //starts 
 import java.util.*;
@@ -9,12 +10,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b= scan.nextInt();
-        int c=scan.nextInt();
       
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        
     }
 }
